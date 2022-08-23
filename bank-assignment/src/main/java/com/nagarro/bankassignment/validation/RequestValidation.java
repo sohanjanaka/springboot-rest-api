4de130a865124eed60ae343ee5530c9c5025ff34
sohanjanaka <sohanjanaka@sohanjanakas-MacBook-Air.local>
2022-08-24 00:15:23 +0530

    validation and exception handling

--- bank-assignment/src/main/java/com/nagarro/bankassignment/validation/RequestValidation.java
+++ bank-assignment/src/main/java/com/nagarro/bankassignment/validation/RequestValidation.java
@@ -5,13 +5,24 @@ import org.springframework.stereotype.Service;
 
 import com.nagarro.bankassignment.entity.Account;
 import com.nagarro.bankassignment.exception.InvalidInputParameterException;
+import com.nagarro.bankassignment.model.StatementRequest;
 import com.nagarro.bankassignment.repository.AccountRepository;
 
 @Service
-public class AccountValidation {
+public class RequestValidation {
 
 	@Autowired
 	private AccountRepository accountRepository;
+
+	public void requestValidation(StatementRequest statementRequest) throws InvalidInputParameterException {
+		if (statementRequest.getMinimumAmount() > statementRequest.getMaximumAmount()) {
+			throw new InvalidInputParameterException("Minimum amaount is grater than Maximun amount");
+		}
+
+		if (statementRequest.getStartDate().compareTo(statementRequest.getEndDate()) > 0) {
+			throw new InvalidInputParameterException("startDate is grater than endDate");
+		}
+	}
 
 	public Account isValidAccount(String accountNumber) throws InvalidInputParameterException {
 		Account account = accountRepository.findByAccountNumber(accountNumber);
