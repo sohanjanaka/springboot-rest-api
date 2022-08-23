027ab4aad8c68cf6bf4e67d125b04efc3a610513
sohanjanaka <sohanjanaka@sohanjanakas-MacBook-Air.local>
2022-08-23 05:32:14 +0530

    fix code smells

--- bank-assignment/src/main/java/com/nagarro/bankassignment/advice/AppExceptionHandler.java
+++ bank-assignment/src/main/java/com/nagarro/bankassignment/advice/AppExceptionHandler.java
@@ -19,9 +19,8 @@ public class AppExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public Map<String, String> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
 		Map<String, String> errorMap = new HashMap<>();
-		ex.getBindingResult().getFieldErrors().forEach(error -> {
-			errorMap.put(error.getField(), error.getDefaultMessage());
-		});
+		ex.getBindingResult().getFieldErrors()
+				.forEach(error -> errorMap.put(error.getField(), error.getDefaultMessage()));
 		return errorMap;
 	}
 
@@ -36,7 +35,7 @@ public class AppExceptionHandler {
 			errorMap.put(ex.getPath().get(0).getFieldName(), ex.getValue() + " is invalid please check the value");
 		}
 
-		if (ex.getTargetType().getName() == "java.time.LocalDate") {
+		if (ex.getTargetType().getName().equals("java.time.LocalDate")) {
 			errorMap.put(ex.getPath().get(0).getFieldName() + "of request body", "Format should be YYYY-MM-DD");
 		}
 
